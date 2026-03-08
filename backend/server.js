22cb77d11bed2494eafe55a6dee6f23351d38d17
EdgarGT041 <edgarguerrerotirado@gmail.com>
2026-03-08 15:35:14 -0700

    feat: add income routes to server for income management

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoute.js';
+import incomeRouter from './routes/incomeRoute.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -16,7 +17,9 @@ app.use(express.urlencoded({ extended: true }));
 connectDB();
 
 //ROUTES
+
 app.use("/api/user", userRouter);
+app.use("/api/income", incomeRouter);
 
 app.get("/", (req, res) => {
     res.send("API WORKING");
